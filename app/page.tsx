10c2a9c55355da9b711a88d71c7aa085f58793b6
Michael Ott <1877-otmici24@users.noreply.hsrt-gitlab-srv.fh-reutlingen.de>
2025-10-14 09:55:04 +0200

    test commit2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-gray-800">Hallo Welt!</h1>
+      <h1 className="text-3xl font-bold text-gray-800">Hallo Welt....</h1>
     </main>
   );
 }
