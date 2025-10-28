6012cc0b01681bf00212b9aa8dd1038ba258adc4
Michael Ott <1877-otmici24@users.noreply.hsrt-gitlab-srv.fh-reutlingen.de>
2025-10-28 01:39:34 +0100

    infographic section concept

--- src/components/sections/Infographic/index.tsx
+++ src/components/sections/Infographic/index.tsx
@@ -1,11 +1,20 @@
+"use client";
+
+import styles from "./Infographic.module.css";
+
 const Infographic = () => {
   return (
-    <section className="py-16 bg-white">
-      <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8">Finanzwissen auf einen Blick</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {/* Hier können später die Infografiken eingefügt werden */}
-        </div>
+    <section className={styles.infografic}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Lorem ipsum dolor sit amet</h2>
+        <p className={styles.subtitle}>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+          <br /> sed diam nonumy eirmod tempor invidunt ut labore et dolore
+        </p>
+        <p className={styles.subsubtitle}>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr
+        </p>
+        <div className={styles.grid}></div>
       </div>
     </section>
   );
