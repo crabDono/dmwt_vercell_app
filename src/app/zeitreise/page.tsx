221d02721abc1d84f895635818b86ed899e814be
Michael Ott <1877-otmici24@users.noreply.hsrt-gitlab-srv.fh-reutlingen.de>
2025-11-30 22:11:38 +0100

    dynamisches refreshen comments

--- src/app/zeitreise/page.tsx
+++ src/app/zeitreise/page.tsx
@@ -2,6 +2,8 @@ import Infographic from "@/src/components/sections/Infographic";
 import CommentSection from "@/src/components/sections/Comment";
 import TextParallax from "@/src/components/features/SlideInText";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <main data-theme="light">
