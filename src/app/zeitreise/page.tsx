1c822f566dd1f7f30c4fab1fe55b317b5845ec75
Michael Ott <1877-otmici24@users.noreply.hsrt-gitlab-srv.fh-reutlingen.de>
2026-01-18 21:13:59 +0100

    debugged

--- src/app/zeitreise/page.tsx
+++ src/app/zeitreise/page.tsx
@@ -1,7 +1,6 @@
 import Infographic from "@/src/components/sections/Infographic";
 import CommentSection from "@/src/components/sections/Comment";
 import TextParallax from "@/src/components/features/SlideInText";
-import TextParallax2 from "@/src/components/features/SlideInText2";
 import FAQ from "@/src/components/sections/FAQ";
 import CryptoInfographic from "@/src/components/charts/Infographic";
 import Zeitreise from "@/src/components/sections/Zeitreise";
