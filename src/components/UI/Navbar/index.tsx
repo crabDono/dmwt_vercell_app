bf9de049b1a5824c639427bb5e011b9a692a73af
Michael Ott <1877-otmici24@users.noreply.hsrt-gitlab-srv.fh-reutlingen.de>
2026-01-18 22:33:01 +0100

    zeitreise responsive

--- src/components/UI/Navbar/index.tsx
+++ src/components/UI/Navbar/index.tsx
@@ -24,7 +24,7 @@ export default function Navbar() {
   const mainLinks = [
     { href: "/", label: "Home" },
     { href: "/quiz", label: "Quiz" },
-    { href: "/zeitreise", label: "Zeitreise" },
+    { href: "/informationen", label: "Informationen" },
   ];
 
   useEffect(() => {
